959bd52f1b6a0f978e1fa673461da5f9d28f196f
DESKTOP-DGFPD6H\SHIN HEEMIN <sinhimin11@naver.com>
2024-03-24 13:21:38 +0900

    TIL : DFS 실전문제 (경쟁적 전염)

--- 취업을위한코딩테스트/dfs_bfs/경쟁적전염.java
+++ 취업을위한코딩테스트/dfs_bfs/경쟁적전염.java
@@ -1,64 +1,85 @@
 package dfs_bfs;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
+class Virus implements Comparable<Virus>{
+	int index;  
+	int second ; 
+	int x; 
+	int y; 
+	
+	Virus(int index, int second, int x, int y){
+		this.index = index ;
+		this.second = second ; 
+		this.x = x ; 
+		this.y = y ; 
+	}
+	@Override 
+	public int compareTo(Virus other) {
+		return this.index - other.index ; 
+	}
+}
+
 public class 경쟁적전염 {
-	public static int n, k, s ; 
-	public static int[][] lab ; 
+	public static int n , k ; 
+	public static int[][] graph = new int[200][200] ; 
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in) ; 
 		n = sc.nextInt() ; 
 		k = sc.nextInt() ; 
 		
-		// making labatory 
-		lab = new int[n][n] ; 
-		for(int i = 0 ; i < n ; i++) 
-			for(int j = 0 ; j < n ;j++)
-				lab[i][j] = sc.nextInt() ; 
-		
-		s = sc.nextInt() ; 
-		virus(1) ;  
-		
-		int x = sc.nextInt() ; 
-		int y = sc.nextInt() ; 
-		System.out.print(lab[y-1][x-1]);
-	}
-	
-	public static boolean virus(int virusNum) {
-		
-		// 종료 조건 
-		if(virusNum > k)
-			return false;
-		
-		// 해당 번호에 해당하는 바이러스 퍼트리기 
+		// 그래프 만들기 & 검사할 바이러스 삽입 
+		ArrayList<Virus> viruses = new ArrayList<>() ; 
 		for(int i = 0 ; i < n ; i++) {
 			for(int j = 0 ; j < n ; j++) {
-				if(lab[i][j] == virusNum) {
-					spreadVirus(virusNum , i, j) ; 
-				}
+				graph[i][j] = sc.nextInt(); 
+	
+				// 전염 시작점 바이러스들 모아놓고 (0초) 
+				if(graph[i][j] != 0)
+					viruses.add(new Virus(graph[i][j], 0 , i, j)); 
 			}
-		} 
-		return virus(virusNum+1) ; 
+		}
+		Collections.sort(viruses) ; // 낮은 번호 바이러스부터 
+		
+		int s = sc.nextInt() ; 
+		bfs(viruses,s) ; 
+
+		int x = sc.nextInt() ; 
+		int y = sc.nextInt() ; 
+		System.out.println(graph[x-1][y-1]) ; 
 	}
 	
-	
-	public static void spreadVirus(int virusNum, int x, int y) {
-		// bfs -> 가능한 끝까지 퍼트리는게 아닌 가까운 쪽부터 증식하다가 중간에 끊길 수도 있으므로 bfs로 처리해줘야함 
+	public static void bfs(ArrayList<Virus> viruses, int s) {
 		
-		int[] dx = {1, -1, 0, 0} ; 
-		int[] dy = {0, 0, 1, -1} ;
+		// 큐 만들기 (전염 시작 바이러스들 정렬)  : 1 , 2, 3
+		Queue<Virus> q = new LinkedList<>() ; 
+		for(int i = 0 ; i < viruses.size() ; i++) {
+			q.offer(viruses.get(i)) ; 
+		}
 		
-		for(int i = 0 ; i < 4; i++) {
-			int nx = x + dx[i] ; 
-			int ny = y + dy[i] ; 
+		while(!q.isEmpty()) { // 1 상하좌우 -> 2 상하좌우-> 3 상하좌우-> 1 -> 2 -> 3 -> ... 
+			Virus virus = q.poll() ; 
 			
-			if(nx < 0 || nx >= n || ny < 0 || ny >= n)
-				continue ; 
-			if(lab[nx][ny] == 0) {
-				lab[nx][ny] = virusNum ; 
-				/*시간+1 코드 */ 
-				spreadVirus(virusNum,nx,ny) ; 
+			if(virus.second == s) 
+				break ; 
+			
+			int[] dx = {-1, 1, 0, 0} ; 
+			int[] dy = {0, 0, -1, 1} ;
+			for(int i = 0 ; i < 4 ; i++) { // 1초마다 상하좌우로 증식 
+				int nx = virus.x + dx[i] ; 
+				int ny = virus.y + dy[i] ; 
+				
+				if(nx < 0 || nx >= n || ny < 0 || ny >=n)
+					continue ; 
+				if(graph[nx][ny] == 0) {
+					graph[ny][ny] = virus.index; 
+					q.offer(new Virus(virus.index, virus.second+1 , nx, ny)) ;
+				}
 			}
 		}
 	}
